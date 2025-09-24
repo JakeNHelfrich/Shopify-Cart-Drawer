367fd1ef4f50392d469412c91f9e202b34a4a7e5
Jake Helfrich <jakenichelfrich@gmail.com>
2025-09-24 17:55:49 +0800

    draft: render cart items

--- extensions/custom-cart/assets/custom_cart_drawer.js
+++ extensions/custom-cart/assets/custom_cart_drawer.js
@@ -1,48 +1,43 @@
+class CustomCartUpdatedEvent extends Event {
+    static EVENT_NAME = "custom_cart:updated";
+
+    constructor() {
+        super(CustomCartUpdatedEvent.EVENT_NAME);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const open = window.XMLHttpRequest.prototype.open;
+    window.XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
+        this.addEventListener("readystatechange", () => {
+            if(!url.endsWith("/cart/add.js") && !url.endsWith("/cart/add")) return;
+            if(this.readyState !== XMLHttpRequest.DONE) return;
+            
+            document.dispatchEvent(new CustomCartUpdatedEvent());
+        });
+        open.call(this, method, url, async, user, password);
+    };
+
+    const fetch = window.fetch;
+    window.fetch = function(resource, options) {
+        const response = fetch.call(this, resource, options);
+        response.then(() => {
+            if(resource.endsWith("/cart/add.js") || resource.endsWith("/cart/add")) {
+                document.dispatchEvent(new CustomCartUpdatedEvent());
+            }
+        });
+
+        return response;
+    }
+});
+
 class CustomCartDrawer extends HTMLElement {
     constructor() {
         super();
-
-        const updateCart = this.updateCart.bind(this);
-        const open = window.XMLHttpRequest.prototype.open;
-        window.XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
-            this.addEventListener("readystatechange", () => {
-                if(!url.endsWith("/cart/add.js") && !url.endsWith("/cart/add")) return;
-                if(this.readyState !== XMLHttpRequest.DONE) return;
-               
-                updateCart();
-            });
-            open.call(this, method, url, async, user, password);
-        };
-
-        const fetch = window.fetch;
-        window.fetch = function(resource, options) {
-            const response = fetch.call(this, resource, options);
-            response.then(() => {
-                if(resource.endsWith("/cart/add.js") || resource.endsWith("/cart/add")) {
-                    updateCart();
-                }
-            });
-
-            return response;
-        }
+        document.addEventListener(CustomCartUpdatedEvent.EVENT_NAME, this.updateCart.bind(this))
     }
 
     connectedCallback() {
-        this.setupItemCount();
-        this.setupSlideDrawer();
-    }
-
-    setupItemCount() {
-        const itemCount = Number(this.getAttribute("data-item-count") ?? 0);
-        const itemCountSpan = document.createElement("span");
-        itemCountSpan.id = 'cart-item-count';
-        itemCountSpan.textContent = itemCount;
-
-        this.textContent = "Cart Item Count: "
-        this.appendChild(itemCountSpan)
-    }  
-
-    setupSlideDrawer() {
         let dawnCartDrawerButton = document.querySelector("#cart-icon-bubble");
         if(!dawnCartDrawerButton) return;
 
@@ -58,9 +53,18 @@ class CustomCartDrawer extends HTMLElement {
         const cart = await response.json();
 
         const newItemCount = cart.item_count;
-
         const itemCountSpan = this.querySelector("#cart-item-count");
         itemCountSpan.textContent = newItemCount;
+
+        const items = [];
+        for(const item of cart.items) {
+            const listElement = document.createElement('li');
+            listElement.innerText = `${item.title} | qty: ${item.quantity}`;
+            items.push(listElement);
+        }
+
+        const itemList = this.querySelector("#cart-item-list");
+        itemList.replaceChildren(...items);
     } 
 }
 
